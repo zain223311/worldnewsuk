bd3802b2b6e7faabc2e4d11c322b503e3467aa6b
zain223311 <141604926+zain223311@users.noreply.github.com>
2023-08-07 11:49:46 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://worldnewsuk.online/graphql."
+	const endpoint = "https://worldnewsuk.online/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
